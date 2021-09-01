e9a15d5334802bad9cee91628adda04919a46b77
chjwon <chjwon10044@gmail.com>
2021-09-01 10:47:47 -0500

    08/31 commit

---
+++ Easy/1480. Running Sum of 1d Array.py
@@ -0,0 +1,12 @@
+class Solution:
+    def runningSum(self, nums: List[int]) -> List[int]:
+        sol=[]
+        for i in range(len(nums)):
+            sol.append(sum(nums[:i+1]))
+        return sol
+
+        """
+class Solution:
+    def runningSum(self, nums: List[int]) -> List[int]:
+        return [sum(nums[:i+1]) for i in range(len(nums))]
+        """
---
+++ Easy/1920. Build Array from Permutation.py
@@ -0,0 +1,12 @@
+class Solution:
+    def buildArray(self, nums: List[int]) -> List[int]:
+        sol = []
+        for i in nums:
+            sol.append(nums[i])
+        return sol
+
+        """
+class Solution:
+    def buildArray(self, nums: List[int]) -> List[int]:
+        return [nums[i] for i in nums]
+        """
