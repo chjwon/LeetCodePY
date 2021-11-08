804dbaf60ec57fb5400ea9e4e0c0f43d82d16551
Jae Won Choi <chjwon10044@gmail.com>
2021-11-08 00:02:47 -0600

    11/7

---
+++ Easy/35. Search Insert Position.py
@@ -0,0 +1,12 @@
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        left, right = 0, len(nums) - 1
+        while left <= right:
+            m = (left + right) // 2
+            if nums[m] == target:
+                return m
+            if nums[m] < target:
+                left = m + 1
+            else:
+                right = m - 1
+        return left
