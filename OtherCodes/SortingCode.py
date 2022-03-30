234d986fc25203633224d8a9cfb3b4124420b973
Jae Won Choi <chjwon10044@gmail.com>
2022-03-30 15:24:03 -0500

    3/30

--- OtherCodes/SortingCode.py
+++ OtherCodes/SortingCode.py
@@ -1,7 +1,87 @@
+def bubble_sort(arr):
+    """
+    arr : List[num]
+    return : sorted List
+    """
+    for i in range(len(arr)):
+        for j in range(len(arr)-i-1):
+            if arr[j] > arr[j+1]:
+                arr[j],arr[j+1] = arr[j+1],arr[j]
+    return arr
+
+
+def selection_sort(arr):
+    """
+    arr : List[num]
+    return : sorted List 
+    """
+    for i in range(len(arr)):
+        minIndex = i
+        for j in range(i+1,len(arr)):
+            if arr[j] < arr[i]:
+                minIndex = j
+        if not minIndex == i:
+            arr[i], arr[minIndex] = arr[minIndex], arr[i]
+    return arr
+
+
+def insertion_sort(arr):
+    """
+    arr : List[num]
+    return : sorted List
+    """
+    for i in range(1,len(arr)):
+        num = arr[i]
+        compareTemp = i - 1
+        while compareTemp >= 0 and num < arr[compareTemp]:
+            arr[compareTemp+1] = arr[compareTemp]
+            compareTemp -= 1
+
+        arr[compareTemp + 1] = num
+    return arr
+
+
+def merge(left, right):
+    """
+    sorting arr func
+    used in merge sort
+    """
+    result = []
+    while len(left) > 0 or len(right) > 0:
+        if len(left) > 0 and len(right) > 0:
+            if left[0] <= right[0]:
+                result.append(left[0])
+                left = left[1:]
+            else:
+                result.append(right[0])
+                right = right[1:]
+        elif len(left) > 0:
+            result.append(left[0])
+            left = left[1:]
+        elif len(right) > 0:
+            result.append(right[0])
+            right = right[1:]
+    return result
+
+def merge_sort(arr):
+    """
+    arr : List(num)
+    return recursion with merge func
+    """
+    if len(arr) <= 1:
+        return arr
+    mid = len(arr) // 2
+    left = arr[:mid]
+    right = arr[mid:]
+    left = merge_sort(left)
+    right = merge_sort(right)
+    return merge(left, right)
+
+
 def quick_sort(arr):
     """
     arr : List[num]
-    return : sorted list
+    return : sorted List
     """
     if len(arr) <= 1:
         return arr
@@ -15,6 +95,7 @@ def quick_sort(arr):
         else: # num > pivot
             greaterArr.append(num)
     return quick_sort(lesserArr)+ equalArr + quick_sort(greaterArr)
+
 
 def quick_selection(arr,k):
     """
@@ -40,3 +121,12 @@ def quick_selection(arr,k):
         return pivot
     else: # k th num is in greaterArr
         return quick_selection(greaterArr,k - len(lesserArr) - len(equalArr))
+
+
+
+"""
+testing zone
+"""
+numList = [1,5,2,5,2,3]
+numList = merge_sort(numList)
+print(numList)
